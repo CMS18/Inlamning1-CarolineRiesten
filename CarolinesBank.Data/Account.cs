ed533e9e786d9a7d3626d82a0032b45d627db467
Caroline Riesten <riesten@hotmail.com>
2019-11-13 15:54:32 +0100

    Added classes in new projekt Data

---
+++ CarolinesBank.Data/Account.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CarolinesBank.Data
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ CarolinesBank.Data/BankRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarolinesBank.Data
+{
+    public class BankRepository
+    {
+        public List<Customer> listOfCustomers { get; set; } = new List<Customer>()
+        {
+             new Customer()
+            {
+                 CustomerId = 1,
+                 Name = "Kalle Lind"
+            },
+
+                new Customer()
+            {
+                 CustomerId = 2,
+                 Name = "Fia Persson"
+            },
+
+                   new Customer()
+            {
+                 CustomerId = 3,
+                 Name = "Challe Klasson"
+            }
+        };
+    }
+}
