7270fe1ce0ec6f2a8c34902d3b0214729c066cfe
Caroline Riesten <riesten@hotmail.com>
2019-11-14 10:46:14 +0100

    changed name on testclass

--- CarolinesBank.Test/AccountTests.cs
+++ CarolinesBank.Test/AccountTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace CarolinesBank.Test
 {
     [TestClass]
-    public class UnitTest1
+    public class AccountTests
     {
         [TestMethod]
         public void TestMethod1()
