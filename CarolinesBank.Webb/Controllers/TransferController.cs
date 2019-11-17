f65f8c0862a0b4e0b7fc224318da5184783fb800
Ellen Demérus <ellendemerus@hotmail.com>
2019-11-17 01:14:47 +0100

    Created TransferController + View + ViewModel

---
+++ CarolinesBank.Webb/Controllers/TransferController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarolinesBank.Data;
+using CarolinesBank.Webb.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CarolinesBank.Webb.Controllers
+{
+    public class TransferController : Controller
+    {
+        private readonly BankRepository repo;
+        private readonly TransferViewModel vm;
+        public TransferController(BankRepository repo)
+        {
+            this.repo = repo;
+            this.vm = new TransferViewModel();
+        }
+        public IActionResult Index()
+        {
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult HandleTransfer(TransferViewModel vm)
+        {
+            var fromAccount = repo.FindAccount(vm.FromAccountId);
+            if (fromAccount == null)
+            {
+                ViewBag.Message = "The account doesn't exist";
+                return View("Index");
+            }
+            else
+            {
+                var toAccount = repo.FindAccount(vm.ToAccountId);
+                if (toAccount == null)
+                {
+                    ViewBag.Message = "The account doesn't exist";
+                    return View("Index");
+                }
+                else
+                {
+                    //TODO: koll för minusbelopp och för höga belopp
+                    //använda redan skriven kod?
+                    //antagligen för många if satser i varann, Göra try-catch istället?
+                }
+            }
+        }
+    }
+}
---
+++ CarolinesBank.Webb/ViewModels/TransferViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using CarolinesBank.Webb.Models;
+
+namespace CarolinesBank.Webb.ViewModels
+{
+    public class TransferViewModel
+    {
+        public int FromAccountId { get; set; }
+        public int ToAccountId { get; set; }
+
+        [Display(Name = "Belopp")]
+        [Range(0.01, 9999999.99, ErrorMessage = "The amount must be between 0.01 - 9 999 999,99")]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+    }
+}
